9cebc0898d836a75c361be44b663229136db1287
Jeyavarma <Jeyavarma@email.com>
2026-01-28 18:13:18 +0530

    Disable CSS optimization to fix build error

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -14,7 +14,6 @@ const nextConfig = {
   generateEtags: false,
   experimental: {
     optimizePackageImports: ['@tensorflow/tfjs', '@tensorflow-models/mobilenet'],
-    optimizeCss: true,
   },
   turbopack: {},
   async rewrites() {
